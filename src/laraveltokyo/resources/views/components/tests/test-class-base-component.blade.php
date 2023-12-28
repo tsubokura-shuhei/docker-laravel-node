9b5f703f2178fedb756b33ac3ca4cc0668c61cb7
tsubokura-shuhei <repeat2639@gmail.com>
2023-12-28 10:46:49 +0900

    component cleat

---
+++ src/laraveltokyo/resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネント
+    <div>{{ $classBaseMessage }}</div>
+    <!-- Let all your things have their places; let each part of your business have its time. - Benjamin Franklin -->
+    <div>{{$defaultMessage}}</div>
+</div>
